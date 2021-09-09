c65cd9f9c46fbd26e025269f84cd84775f69d0cf
M'ary Miller <mcmiller@Marys-iMac.attlocal.net>
2021-09-08 20:59:53 -0500

    part two and three complete

--- db/seed.js
+++ db/seed.js
@@ -8,6 +8,10 @@ const {
   updatePost,
   getAllPosts,
   getPostsByUser,
+  addTagsToPost,
+  createTags,
+  createPostTag,
+  getPostById,
 } = require("./index");
 
 async function dropTables() {
@@ -16,11 +20,11 @@ async function dropTables() {
 
     // have to make sure to drop in correct order
     await client.query(`
-    DROP TABLE IF EXISTS post_tags;
-    DROP TABLE IF EXISTS tags;
-    DROP TABLE IF EXISTS posts;
-    DROP TABLE IF EXISTS users;
-  `);
+      DROP TABLE IF EXISTS post_tags;
+      DROP TABLE IF EXISTS tags;
+      DROP TABLE IF EXISTS posts;
+      DROP TABLE IF EXISTS users;
+    `);
 
     console.log("Finished dropping tables!");
   } catch (error) {
@@ -48,6 +52,15 @@ async function createTables() {
         title varchar(255) NOT NULL,
         content TEXT NOT NULL,
         active BOOLEAN DEFAULT true
+      );
+      CREATE TABLE tags (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL
+      );
+      CREATE TABLE post_tags (
+        "postId" INTEGER REFERENCES posts(id),
+        "tagId" INTEGER REFERENCES tags(id),
+        UNIQUE("postId","tagId")
       );
     `);
 
@@ -98,18 +111,21 @@ async function createInitialPosts() {
       title: "First Post",
       content:
         "This is my first post. I hope I love writing blogs as much as I love writing them.",
+      tags: ["#happy", "#youcandoanything"],
     });
 
     await createPost({
       authorId: sandra.id,
       title: "How does this work?",
       content: "Seriously, does this even do anything?",
+      tags: ["#happy", "#worst-day-ever"],
     });
 
     await createPost({
       authorId: glamgal.id,
       title: "Living the Glam Life",
       content: "Do you even? I swear that half of you are posing.",
+      tags: ["#happy", "#youcandoanything", "#canmandoeverything"],
     });
     console.log("Finished creating posts!");
   } catch (error) {
